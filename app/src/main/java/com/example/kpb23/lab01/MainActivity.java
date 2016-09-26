aceeaf8d97ee8ff9b9effdf982b5f8cc26423e90
Krisofer Brink <kpgbrink@gmail.com>
2016-09-26 19:54:40 -0400

    Fixed what needs to be printed out

--- app/src/main/java/com/example/kpb23/lab01/MainActivity.java
+++ app/src/main/java/com/example/kpb23/lab01/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.kpb23.lab01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/** This is a test app
+ *
+ * For use as CS 262, Lab 1
+ *
+ * @author Kristofer
+ * @version Fall, 2016
+ */
 
 public class MainActivity extends AppCompatActivity {
 
